87846fb4dd740150c892463d99aee1cfc750b8f2
Julian StJames <julianstjames@gmail.com>
2023-03-31 23:37:35 -0400

    finishes hw

--- homework.js
+++ homework.js
@@ -1,89 +1,101 @@
 // 1) Sum the Numbers
-let sum = [11, 23, 34, 54, 57];
-let num = 0;
-for (i = 0; i < sum.length; i++) {
-    num = num + sum[i];
-}
-console.log(num);
+// let sum = [11, 23, 34, 54, 57];
+// let num = 0;
+// for (i = 0; i < sum.length; i++) {
+//     num = num + sum[i];
+// }
+// console.log(num);
 
-// 2) Find the largest element in an array and print to the console
-const largest = [5, 7, 2, 4, 9];
-let largeElement = 0
-for (let i = 0; i < largest.length; i++) {
-    if (largeElement < largest[i]) {
-        largeElement = largest[i];
-    }
-}
-console.log(largeElement);
+// // 2) Find the largest element in an array and print to the console
+// const largest = [5, 7, 2, 4, 9];
+// let largeElement = 0
+// for (let i = 0; i < largest.length; i++) {
+//     if (largeElement < largest[i]) {
+//         largeElement = largest[i];
+//     }
+// }
+// console.log(largeElement);
 
-// 3) Find the smallest element in an array and print to the console
-const small = [5, 7, 2, 4, 9];
-let lil = small[0];
-for (let i = 0; i < small.length; i++) {
-    if (lil > small[i]) {
-        lil = small[i];
-    }
-}
-console.log(lil)
+// // 3) Find the smallest element in an array and print to the console
+// const small = [5, 7, 2, 4, 9];
+// let lil = small[0];
+// for (let i = 0; i < small.length; i++) {
+//     if (lil > small[i]) {
+//         lil = small[i];
+//     }
+// }
+// console.log(lil)
 
-// 4) For the given array of numbers, print each number in array that is even.
-const even = [34, 46, 43, 31, 87, 57, 55, 84, 12, 6, 98, 77];
-for (let i = 0; i < even.length; i++) {
-    if (even[i] % 2 == 0) {
-        console.log(even[i])
-    }
-}
+// // 4) For the given array of numbers, print each number in array that is even.
+// const even = [34, 46, 43, 31, 87, 57, 55, 84, 12, 6, 98, 77];
+// for (let i = 0; i < even.length; i++) {
+//     if (even[i] % 2 == 0) {
+//         console.log(even[i])
+//     }
+// }
 
-// 5) For the given array of numbers, print each number in the array that is greater than zero.
-const positive = [4, -86, 16, 67, -48, -6, -8, 12, 31, 78, -3, -2, 7, -4];
-for (let i = 0; x < positive.length; x++) {
-    if (positive[x] > 0) {
-        console.log(positive[x]);
-    }
-}
+// // 5) For the given array of numbers, print each number in the array that is greater than zero.
+// const positive = [4, -86, 16, 67, -48, -6, -8, 12, 31, 78, -3, -2, 7, -4];
+// for (let i = 0; x < positive.length; x++) {
+//     if (positive[x] > 0) {
+//         console.log(positive[x]);
+//     }
+// }
 
-// 6) For the given array of numbers, create a new array which contains every number in the given array which is positive
-// const positive2 = [34, -56, 16, 23, -4, -6, -8, 12, 11, 78, -3, -2, 5, -44 ];
-let anArray = [];
-for (let x = 0; x < positive2.length; x++) {
-    if (positive2[x] > 0) {
-        anArray.push(positive2[x]);
-    }
-}
-console.log(anArray)
+// // 6) For the given array of numbers, create a new array which contains every number in the given array which is positive
+// // const positive2 = [34, -56, 16, 23, -4, -6, -8, 12, 11, 78, -3, -2, 5, -44 ];
+// let anArray = [];
+// for (let x = 0; x < positive2.length; x++) {
+//     if (positive2[x] > 0) {
+//         anArray.push(positive2[x]);
+//     }
+// }
+// console.log(anArray)
 
-// 7) Given an array of strings, print the ones that start with the letter 'A'.
-const strings = ['Apple', 'Banana', 'Apricot', 'Cherry', 'Avocado'];
-for (let x = 0; x < strings.length; x++) {
-    if (strings[x][0] === 'A') {
-        console.log(strings[x]);
-    }
-}
+// // 7) Given an array of strings, print the ones that start with the letter 'A'.
+// const strings = ['Apple', 'Banana', 'Apricot', 'Cherry', 'Avocado'];
+// for (let x = 0; x < strings.length; x++) {
+//     if (strings[x][0] === 'A') {
+//         console.log(strings[x]);
+//     }
+// }
 
-// 8)  Given an array of strings, print the longest one.
-const strings = ['short', 'longer', 'longest', 'medium'];
-let newString = strings[0]
-for (let i = 0; i < strings.length; i++) {
-    if (newString.length < strings[i].length) {
-        newString = strings[i];
-    }
-    else {
-        newString = newString;
-    }
-}
-console.log(newString);
+// // 8)  Given an array of strings, print the longest one.
+// const strings = ['short', 'longer', 'longest', 'medium'];
+// let newString = strings[0]
+// for (let i = 0; i < strings.length; i++) {
+//     if (newString.length < strings[i].length) {
+//         newString = strings[i];
+//     }
+//     else {
+//         newString = newString;
+//     }
+// }
+// console.log(newString);
 
 // 9) For the given array of numbers, and a factor of 5, create a new array consisting of each of the numbers in the array multiplied by the factor. Print this array.
 
+// const positive2 = [34, -56, 16, 23, -4, -6, -8, 12, 11, 78, -3, -2, 5, -44]
+
+// const theResult = positive2.map(x => {
+//     return (x * 5)
+// })
+// console.log(theResult)
+
+
 
 // 10) Reverse a String
-let string = 'Hello World';
-let reverseString = ''
-for (let i = string.length - 1; i >= 0; i--) {
-    reverseString += string[i]
-}
-console.log(reverseString)
+
+
+// let string = 'Hello World';
+// let reverseString = ''
+// for (let i = string.length - 1; i >= 0; i--) {
+//     reverseString += string[i]
+// }
+// console.log(reverseString)
 
 // 11) Multiply Vectors
+
+
 
 // 12) Matrix Addition
